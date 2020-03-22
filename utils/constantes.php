9c3ff2c52a1886f15d770bb52bf80aea4faba563
Gosuhan <david.sylvestre.lp2@gmail.com>
2020-03-22 22:19:06 +0100

    ajout ajout com

--- utils/constantes.php
+++ utils/constantes.php
@@ -1,8 +1,8 @@
 <?php
 // afin d'assurer une meilleure maintenabilité, j'ai défini des constantes pour ma bdd, ainsi s'il y a une valeur à changer, on évite de devoir le faire partout.
-define('USER_DB', 'root');
-define('MDP_DB', '0000');
-define('DB_NAME', 'memory_db');
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
+define('DB_NAME', 'memory_db');
+define('DB_USER', 'root');
+define('DB_MDP', '0000');
 ?>
